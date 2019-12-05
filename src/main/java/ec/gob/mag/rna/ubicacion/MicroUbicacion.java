3d01d1aa431678470b11ff662f6564e92b6a2c8e
Darwin Stalin Ramirez Supe <dramirez@mag.gob.ec>
2019-12-05 08:37:05 -0500

    con tokens 52

--- src/main/java/ec/gob/mag/rna/ubicacion/MicroUbicacion.java
+++ src/main/java/ec/gob/mag/rna/ubicacion/MicroUbicacion.java
@@ -8,11 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableResourceServer
 @EnableSwagger2
 @SpringBootApplication
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MicroUbicacion extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
